182f02d041f4361285b7ea1dbc8f7b2d9c14811e
peiyunwu <zoeyunwu@gmail.com>
2018-05-26 13:00:26 +0800

    20180525

--- js/main.js
+++ js/main.js
@@ -29,18 +29,20 @@ $(document).ready(function(){
 
 $("#1flo").on('click', function() {
 
-  $.fancybox.open('<div class="message"><p>你/妳也太節儉了！不過有跟上流行喔~</p><img src="https://api.fnkr.net/testimg/600x400/00CED1/FFF/?text=img+placeholder"></div>');
+  $.fancybox.open('<div class="message"><p>你/妳也太節儉了！不過有跟上流行喔~</p></div>');
 
 });
 
 $("#2flo").on('click', function() {
 
-  $.fancybox.open('<div class="message"><p>一束花</p><img src="https://api.fnkr.net/testimg/600x400/00CED1/FFF/?text=img+placeholder"></div>');
+  $.fancybox.open('<div class="message"><p>這是個不錯的選擇，不過可以有點新意喔~</p></div>');
 
 });
 
 $("#3flo").on('click', function() {
 
-  $.fancybox.open('<div class="message"><p>看來你/妳是個浪漫的人</p><img src="https://api.fnkr.net/testimg/600x400/00CED1/FFF/?text=img+placeholder"></div>');
+  $.fancybox.open('<div class="message"><p>看來你/妳是個浪漫的人！也許你/妳準備告白或求婚？</p></div>');
 
 });
+
+// banner
