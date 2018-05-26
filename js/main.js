9c148bd61595d71413c4638b24acb64d7de7b35c
peiyunwu <zoeyunwu@gmail.com>
2018-05-27 00:03:13 +0800

    20180526

--- js/main.js
+++ js/main.js
@@ -55,26 +55,74 @@ $("#no").on('click', function() {
 
 $("#lily").on('click', function() {
 
-  $.fancybox.open('<div class="message"><h3>百合</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quaerat!</p></div>');
+  $.fancybox.open('<div class="message"><h3>百合花</h3><p>花語：喜歡你的那種純真</p></div>');
 
 });
 
 $("#sun").on('click', function() {
 
-  $.fancybox.open('<div class="message"><h3>向日葵</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quaerat!</p></div>');
+  $.fancybox.open('<div class="message"><h3>向日葵</h3><p>花語：我深深愛慕著你</p></div>');
 
 });
 
 $("#orchid").on('click', function() {
 
-  $.fancybox.open('<div class="message"><h3>小蒼蘭</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quaerat!</p></div>');
+  $.fancybox.open('<div class="message"><h3>小蒼蘭</h3><p>花語：純潔、幸福、清新舒暢</p></div>');
 
 });
 
 $("#rose").on('click', function() {
 
-  $.fancybox.open('<div class="message"><h3>玫瑰</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quaerat!</p></div>');
+  $.fancybox.open('<div class="message"><h3>玫瑰</h3><p>紅/粉玫瑰是情人節的首選花卉。<br>花語：紅玫瑰代表熱戀，真心真意；粉紅玫瑰代表初戀、特別的關懷</p></div>');
 
 });
 
-// banner
+// other
+
+$("#ever").on('click', function() {
+
+  $.fancybox.open('<div class="message"><h3>不凋花</h3><p>在花朵最新鮮、美麗時採集下來，經過乾燥脫水後，以保鮮液取代水分，讓花朵維持彈性及柔軟的觸感。特殊加工後，可保存半年至三年以上，且不需澆水。由於沒有香味與花粉，也不用擔心是否會過敏。</p></div>');
+
+});
+
+$("#dry").on('click', function() {
+
+  $.fancybox.open('<div class="message"><h3>乾燥花</h3><p>分為工業大量生產、自然風乾製成及天然素材拼接等。其中的水分會完全被抽除，尺寸及顏色也會改變，可經由加工製成多種顏色和造型，而部分花材仍能散發出天然香味。</p></div>');
+
+});
+
+$("#soap").on('click', function() {
+
+  $.fancybox.open('<div class="message"><h3>香皂花</h3><p>經常製作成各種花朵造型，同時具備觀賞性與實用性，是送禮的好選擇。</p></div>');
+
+});
+
+$("#cactus").on('click', function() {
+
+  $.fancybox.open('<div class="message"><h3>多肉植物</h3><p>其中也有花語代表愛情的品種，例：帕米爾玫瑰、山地玫瑰。</p></div>');
+
+});
+
+$("#bear").on('click', function() {
+
+  $.fancybox.open('<div class="message"><h3>玩偶花束</h3><p>將普通花束中的花替代為玩偶，例：小熊。由於不易損壞、保存時間長，成為新型的送禮方式。</p></div>');
+
+});
+
+$("#gift").on('click', function() {
+
+  $.fancybox.open('<div class="message"><h3>花禮</h3><p>以花搭配各式產品，例：香水、保養品。</p></div>');
+
+});
+
+$("#berry").on('click', function() {
+
+  $.fancybox.open('<div class="message"><h3>草莓花束</h3><p>近年韓國流行起來的風潮，由於具季節性、可食用與特殊的水果香深受喜愛。</p></div>');
+
+});
+
+$("#money").on('click', function() {
+
+  $.fancybox.open('<div class="message"><h3>鈔票花束</h3><p>別具一格的創意，將紙鈔折成花朵造型送人。</p></div>');
+
+});
